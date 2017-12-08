openstack/openstacksdk

Make the get_service_type() overrides tolernat of no defaults
The service type overrides in get_service_type() fail if the
API version keys are not present in the config dict, which happens
when CloudConfig is created without reading defaults.

Cherry-picked from I8d035cfd1afc1cad01ceac7cd643568e94897e27

Change-Id: I8d035cfd1afc1cad01ceac7cd643568e94897e27

openstack/config/cloud_config.py
@@ -169,12 +169,14 @@ class CloudConfig(object):
         # TODO(shade) This should get removed when we have os-service-types
         # alias support landed in keystoneauth.
         if service_type in ('volume', 'block-storage'):
-            if self.get_api_version('volume').startswith('2'):
+            vol_ver = self.get_api_version('volume')
+            if vol_ver and vol_ver.startswith('2'):
                 service_type = 'volumev2'
-            elif self.get_api_version('volume').startswith('3'):
+            elif vol_ver and vol_ver.startswith('3'):
                 service_type = 'volumev3'
         elif service_type == 'workflow':
-            if self.get_api_version(service_type).startswith('2'):
+            wk_ver = self.get_api_version(service_type)
+            if wk_ver and wk_ver.startswith('2'):
                 service_type = 'workflowv2'
         return self.config.get(key, service_type)
 

openstack/os-client-config

os_client_config/cloud_config.py
@@ -167,12 +167,14 @@ class CloudConfig(object):
         # What's even more amazing is that they did it AGAIN with cinder v3
         # And then I learned that mistral copied it.
         if service_type == 'volume':
-            if self.get_api_version(service_type).startswith('2'):
+            vol_type = self.get_api_version(service_type)
+            if vol_type and vol_type.startswith('2'):
                 service_type = 'volumev2'
-            elif self.get_api_version(service_type).startswith('3'):
+            elif vol_type and vol_type.startswith('3'):
                 service_type = 'volumev3'
         elif service_type == 'workflow':
-            if self.get_api_version(service_type).startswith('2'):
+            wk_type = self.get_api_version(service_type)
+            if wk_type and wk_type.startswith('2'):
                 service_type = 'workflowv2'
         return self.config.get(key, service_type)
 