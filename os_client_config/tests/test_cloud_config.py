6d5283eb9af5b43307d91f1bc3a9d72ecce7df4b
Stephen Finucane <stephenfin@redhat.com>
2025-05-30 13:39:06 +0100

    tests: Remove duplicated tests
    We should not be testing functionality from the parent class,
    particularly when they're duplicating tests already found in the parent
    library (openstack/tests/unit/config/test_cloud_config.py).
    
    Signed-off-by: Stephen Finucane <stephenfin@redhat.com>
    Change-Id: Idba69929e020c487f98d16ec7adaf69bf98d2f30

--- os_client_config/tests/test_cloud_config.py
+++ os_client_config/tests/test_cloud_config.py
@@ -10,17 +10,12 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-import copy
 from unittest import mock
-
-from keystoneauth1 import exceptions as ksa_exceptions
-from keystoneauth1 import session as ksa_session
 
 from openstack.config import cloud_region
 
 from os_client_config import cloud_config
 from os_client_config import defaults
-from os_client_config import exceptions
 from os_client_config.tests import base
 
 
@@ -39,170 +34,6 @@ fake_services_dict = {
 
 
 class TestCloudConfig(base.TestCase):
-
-    def test_arbitrary_attributes(self):
-        cc = cloud_config.CloudConfig("test1", "region-al", fake_config_dict)
-        self.assertEqual("test1", cc.name)
-        self.assertEqual("region-al", cc.region)
-
-        # Look up straight value
-        self.assertEqual('1', cc.a)
-
-        # Look up prefixed attribute, fail - returns None
-        self.assertIsNone(cc.os_b)
-
-        # Look up straight value, then prefixed value
-        self.assertEqual('3', cc.c)
-        self.assertEqual('3', cc.os_c)
-
-        # Lookup mystery attribute
-        self.assertIsNone(cc.x)
-
-        # Test default ipv6
-        self.assertFalse(cc.force_ipv4)
-
-    def test_iteration(self):
-        cc = cloud_config.CloudConfig("test1", "region-al", fake_config_dict)
-        self.assertIn('a', cc)
-        self.assertNotIn('x', cc)
-
-    def test_equality(self):
-        cc1 = cloud_config.CloudConfig("test1", "region-al", fake_config_dict)
-        cc2 = cloud_config.CloudConfig("test1", "region-al", fake_config_dict)
-        self.assertEqual(cc1, cc2)
-
-    def test_inequality(self):
-        cc1 = cloud_config.CloudConfig("test1", "region-al", fake_config_dict)
-
-        cc2 = cloud_config.CloudConfig("test2", "region-al", fake_config_dict)
-        self.assertNotEqual(cc1, cc2)
-
-        cc2 = cloud_config.CloudConfig("test1", "region-xx", fake_config_dict)
-        self.assertNotEqual(cc1, cc2)
-
-        cc2 = cloud_config.CloudConfig("test1", "region-al", {})
-        self.assertNotEqual(cc1, cc2)
-
-    def test_verify(self):
-        config_dict = copy.deepcopy(fake_config_dict)
-        config_dict['cacert'] = None
-
-        config_dict['verify'] = False
-        cc = cloud_config.CloudConfig("test1", "region-xx", config_dict)
-        (verify, cert) = cc.get_requests_verify_args()
-        self.assertFalse(verify)
-
-        config_dict['verify'] = True
-        cc = cloud_config.CloudConfig("test1", "region-xx", config_dict)
-        (verify, cert) = cc.get_requests_verify_args()
-        self.assertTrue(verify)
-
-    def test_verify_cacert(self):
-        config_dict = copy.deepcopy(fake_config_dict)
-        config_dict['cacert'] = "certfile"
-
-        config_dict['verify'] = False
-        cc = cloud_config.CloudConfig("test1", "region-xx", config_dict)
-        (verify, cert) = cc.get_requests_verify_args()
-        self.assertFalse(verify)
-
-        config_dict['verify'] = True
-        cc = cloud_config.CloudConfig("test1", "region-xx", config_dict)
-        (verify, cert) = cc.get_requests_verify_args()
-        self.assertEqual("certfile", verify)
-
-    def test_cert_with_key(self):
-        config_dict = copy.deepcopy(fake_config_dict)
-        config_dict['cacert'] = None
-        config_dict['verify'] = False
-
-        config_dict['cert'] = 'cert'
-        config_dict['key'] = 'key'
-
-        cc = cloud_config.CloudConfig("test1", "region-xx", config_dict)
-        (verify, cert) = cc.get_requests_verify_args()
-        self.assertEqual(("cert", "key"), cert)
-
-    def test_ipv6(self):
-        cc = cloud_config.CloudConfig(
-            "test1", "region-al", fake_config_dict, force_ipv4=True)
-        self.assertTrue(cc.force_ipv4)
-
-    def test_getters(self):
-        cc = cloud_config.CloudConfig("test1", "region-al", fake_services_dict)
-
-        self.assertEqual(['compute', 'identity', 'image', 'volume'],
-                         sorted(cc.get_services()))
-        self.assertEqual({'password': 'hunter2', 'username': 'AzureDiamond'},
-                         cc.get_auth_args())
-        self.assertEqual('public', cc.get_interface())
-        self.assertEqual('public', cc.get_interface('compute'))
-        self.assertEqual('admin', cc.get_interface('identity'))
-        self.assertEqual('region-al', cc.get_region_name())
-        self.assertEqual('region-al', cc.get_region_name('image'))
-        self.assertIsNone(cc.get_api_version('image'))
-        self.assertEqual('2', cc.get_api_version('compute'))
-        self.assertEqual('mage', cc.get_service_type('image'))
-        self.assertEqual('compute', cc.get_service_type('compute'))
-        self.assertEqual('1', cc.get_api_version('volume'))
-        self.assertIn(
-            cc.get_service_type('volume'), ['block-storage', 'volume'])
-        self.assertEqual('http://compute.example.com',
-                         cc.get_endpoint('compute'))
-        self.assertIsNone(cc.get_endpoint('image'))
-        self.assertIsNone(cc.get_service_name('compute'))
-        self.assertEqual('locks', cc.get_service_name('identity'))
-
-    def test_get_session_no_auth(self):
-        config_dict = defaults.get_defaults()
-        config_dict.update(fake_services_dict)
-        cc = cloud_config.CloudConfig("test1", "region-al", config_dict)
-        self.assertRaises(
-            exceptions.OpenStackConfigException,
-            cc.get_session)
-
-    @mock.patch.object(ksa_session, 'Session')
-    def test_override_session_endpoint_override(self, mock_session):
-        config_dict = defaults.get_defaults()
-        config_dict.update(fake_services_dict)
-        cc = cloud_config.CloudConfig(
-            "test1", "region-al", config_dict, auth_plugin=mock.Mock())
-        self.assertEqual(
-            cc.get_session_endpoint('compute'),
-            fake_services_dict['compute_endpoint_override'])
-
-    @mock.patch.object(ksa_session, 'Session')
-    def test_override_session_endpoint(self, mock_session):
-        config_dict = defaults.get_defaults()
-        config_dict.update(fake_services_dict)
-        cc = cloud_config.CloudConfig(
-            "test1", "region-al", config_dict, auth_plugin=mock.Mock())
-        self.assertEqual(
-            cc.get_session_endpoint('telemetry'),
-            fake_services_dict['telemetry_endpoint'])
-
-    @mock.patch.object(cloud_region.CloudRegion, 'get_session')
-    def test_session_endpoint(self, mock_get_session):
-        mock_session = mock.Mock()
-        mock_get_session.return_value = mock_session
-        config_dict = defaults.get_defaults()
-        config_dict.update(fake_services_dict)
-        cc = cloud_config.CloudConfig(
-            "test1", "region-al", config_dict, auth_plugin=mock.Mock())
-        cc.get_session_endpoint('orchestration')
-        mock_session.get_endpoint.assert_called_with(
-            interface='public',
-            service_name=None,
-            region_name='region-al',
-            service_type='orchestration')
-
-    @mock.patch.object(cloud_region.CloudRegion, 'get_session')
-    def test_session_endpoint_not_found(self, mock_get_session):
-        exc_to_raise = ksa_exceptions.catalog.EndpointNotFound
-        mock_get_session.return_value.get_endpoint.side_effect = exc_to_raise
-        cc = cloud_config.CloudConfig(
-            "test1", "region-al", {}, auth_plugin=mock.Mock())
-        self.assertIsNone(cc.get_session_endpoint('notfound'))
 
     @mock.patch.object(cloud_region.CloudRegion, 'get_api_version')
     @mock.patch.object(cloud_region.CloudRegion, 'get_auth_args')
